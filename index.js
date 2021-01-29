1ef429add1a87195eb0895faacdcbc5802e6b40d
Jack Henry <jackson.lewis.henry@gmail.com>
2021-01-29 13:31:00 -0500

    done with mvp

--- index.js
+++ index.js
@@ -209,7 +209,17 @@ CuboidMaker.prototype.volume = function(){
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo{
-
+  constructor(cuboidTwo){
+    this.length = cuboidTwo.length;
+    this.width = cuboidTwo.width;
+    this.height = cuboidTwo.height;
+  }
+  volume(){
+    return this.length * this.width * this.height;
+  }
+  surfaceArea(){
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
 }
 
 
